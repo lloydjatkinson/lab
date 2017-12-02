efbdc5770527b34abc2cbb3eeb712b9110769277
Lloyd Atkinson <lloydjatkinson@gmail.com>
2017-12-02 23:46:05 +0000

    Stuck

--- vue-experiments/src/components/computed-toggle.vue
+++ vue-experiments/src/components/computed-toggle.vue
@@ -3,13 +3,17 @@
         <table>
             <thead>
                 <tr>
-                    <th v-for="step in timesteps.length" v-bind:key="step">{{ timestepStart + step}}</th>
+                    <!-- <th v-for="(step, index) in timeSteps.length" v-bind:key="step">{{ timeStepLabelCalculation(index) }}</th> -->
+                    <th
+                        v-for="(timeStep, index) in timeStepVariationCalculation(timeStepStart, timeStepIncrement, timeStepCount)"
+                        v-bind:key="timeStep">
+                        {{ timeStep }}</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td
-                        v-for="(item, index) in timesteps"
+                        v-for="(item, index) in timeSteps"
                         v-bind:key="index">
                         <div
                             v-bind:class="{ selected: item.selected }"
@@ -19,7 +23,13 @@
                 </tr>
             </tbody>
         </table>
-    <p>You have selected {{ selectedItems }}</p>
+        <div>
+            <p>{{ selectedTimeSteps.length }} selected timeSteps:</p>
+            <ul>
+                <!-- <li v-for="(item, index) in selectedTimeSteps" v-bind:key="index">{{ timeSteps[index] }}</li> -->
+            </ul>
+        </div>
+        <button v-on:click="reactiveTest()">Change timestep start</button>
     </div>
 </template>
 
@@ -27,18 +37,19 @@
 // @ts-check
 
 let temporaryViewModel = {
-    timestepStart: 1901,
-    timestepIncrement: 1,
+    timeStepStart: 1901,
+    timeStepIncrement: 5,
     optionTimings: [
+        false,
+        true,
+        false,
+        false,
+        true,
+        false,
         true,
         true,
         true,
-        true,
-        true,
-        true,
-        true,
-        true,
-        true,
+        false,
         true
     ]
 };
@@ -47,30 +58,47 @@ export default {
     name: 'ComputedToggle',
     data () {
         return {
-            timesteps: [],
-            timestepStart: 0,
-            timestepIncrement: 0
+            timeSteps: [],
+            timeStepStart: 0,
+            timeStepIncrement: 0,
+            timeStepCount: 0
         }
     },
     computed: {
-        selectedItems: function () {
-            return this.timesteps.filter(timestep => timestep.selected);
+        selectedTimeSteps () {
+            return this.timeSteps.filter(timestep => timestep.selected);
         }
     },
     methods: {
-        toggleSelectedItem: function (timestepIndex) {
-            this.timesteps[timestepIndex].selected = !this.timesteps[timestepIndex].selected;
+        toggleSelectedItem (timestepIndex) {
+            this.timeSteps[timestepIndex].selected = !this.timeSteps[timestepIndex].selected;
+        },
+        timeStepLabelCalculation (index) {
+            return index === 0 ? this.timeStepStart : this.timeStepStart + index * this.timeStepIncrement
+        },
+        timeStepVariationCalculation (initialTimeStep, variation, numberOfTimeSteps) {
+            let timeSteps = [];
+            for (let i = 0; i < numberOfTimeSteps; i++) {
+                timeSteps.push(initialTimeStep + i * variation);
+            }
+
+            return timeSteps;
+        },
+        reactiveTest () {
+            this.timeStepStart = 2000;
         }
     },
     created () {
-        this.timesteps = temporaryViewModel.optionTimings.map((x) => {
+        this.timeStepStart = temporaryViewModel.timeStepStart;
+        this.timeStepIncrement = temporaryViewModel.timeStepIncrement;
+        this.timeStepCount = temporaryViewModel.optionTimings.length;
+
+        this.timeSteps = temporaryViewModel.optionTimings.map((x, index, array) => {
             return {
-                selected: false,
-                value: x
+                selected: x
+                // value: this.timeStepVariationCalculation(this.timeStepStart, this.timeStepIncrement, array.length)[index]
             }
         });
-        this.timestepStart = temporaryViewModel.timestepStart;
-        this.timestepIncrement = temporaryViewModel.timestepIncrement;
     }
 };
 </script>
